406ed6f51b849d4ef5258b4c70b4e89966c2390d
yunhongyoul <yunhongyoul97@naver.com>
2025-02-13 10:37:52 +0900

    새로운 프로젝트 생성

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,22 @@
+package hello;
+
+/*
+ * 주석 여러 줄
+ * 작성자: 장세이
+ * 작성일: 02/12
+ *  */
+public class HelloJava {
+
+	// 주석이란? 프로그램의 설명, 실제 코드에 영향 x 
+	//	메인함수: 프로그램의 시작점
+	public static void main(String[] args) {
+
+		// 문자를 출력하는 코드
+		// 문장을 완성할때는 세미콜론으로 마무리
+		// 문법에러가 발생이 되면 빨간색으로 표시됨
+		System.out.println("jangsei, hi~");
+				
+		// ctrl + s: 저장 단축키
+	}
+
+}
